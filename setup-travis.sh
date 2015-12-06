35e802e04199e19c68ca42beb65cafc8764715be
silenteh <silenteh@gmail.com>
2015-12-06 18:18:20 +0100

    Added travis for OSX linux clang and gcc

--- setup-travis.sh
+++ setup-travis.sh
@@ -10,7 +10,6 @@ if ( test "`uname -s`" = "Darwin" ); then
   #brew update
   #brew install cmake
   brew install llvm35 && brew link --force llvm35
-  brew install nacl && brew link --force nacl
 else
   echo "Detected LINUX"
   #install a newer cmake since at this time Travis only has version 2.8.7
@@ -19,7 +18,7 @@ else
   sudo add-apt-repository --yes ppa:george-edison55/cmake-3.x
 
   sudo apt-get update -qq
-  sudo apt-get install -y llvm-3.4 llvm-3.4-dev git wget gzip libssl-dev cmake build-essential libnacl-dev clang-3.5
+  sudo apt-get install -y llvm-3.4 llvm-3.4-dev git wget gzip libssl-dev cmake build-essential clang-3.5
   #sudo wget --no-check-certificate https://cmake.org/files/v3.4/cmake-3.4.1.tar.gz
   #sudo tar zxf cmake-3.4.1.tar.gz
   #cd cmake-3.4.1
